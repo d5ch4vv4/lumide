7fd33a2b0adea1e31aeb6dd0721151614aa22e8c
d5ch4vv4 <fresel.rafal@gmail.com>
2024-02-09 11:34:12 +0100

    Add comments to lumide.ino, add notes
    Add comments to lumide.ino in order to understand code.
    Add notes to collect ideas.

--- lumide.ino
+++ lumide.ino
@@ -2,35 +2,43 @@
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
 
-#define LED_PIN 5
+#define LED_PIN 5								// Data Pin
 #define NUM_LEDS 12
 #define BAUD_RATE 9600
 #define EEPROM_ADDRESS 0
 
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];						// Array to set/clear led data
 CRGB color = CRGB::White;
 CRGBPalette16 palette = RainbowColors_p;
-boolean animationRunning = true;
+boolean animationRunning = true;			// USED FOR WHAT AGAIN?
 SoftwareSerial BTSerial(0, 1); // RX | TX
 char lastColor = 'r';
 
 int currentColor = 0;
 int currentLed = 0;
-int currentLed23 = 0;
-int hue = 0;
+int currentLed23 = 0;					// Used in one func, what for?
+int hue = 0;									// hue of red is 0?
 int fadeAmount = 255;
 int fadeAmount23 = 255;
-int Chase_Delay = 50;
-int Color_Delay = 30;
-int delayTime = 30;
+int Chase_Delay = 50;					// Used once?
+int Color_Delay = 30;					// Used once?
+int delayTime = 30;						// Used twice?
 
 void setup() {
+	// Tell lib about:
+	//		- LED strip at DATA_PIN
+	//		- Number of LEDs
+	// The leds will use the 'leds' array
   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);
   BTSerial.begin(BAUD_RATE);
   Serial.begin(BAUD_RATE);
   lastColor = EEPROM.read(EEPROM_ADDRESS);
+  
+  // Set up LEDs after Power On
   switch (lastColor) {
+
+		// static one color
     case 'r':
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Red;
@@ -76,7 +84,11 @@ void setup() {
         leds[i] = CRGB::White;
       }
       break;
+
+		// static two colors
     case 'q':
+
+			// how 0 to 50 if NUM_LEDS < 50?
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 25) {
          leds[i] = CRGB::Yellow;
@@ -136,14 +148,20 @@ void setup() {
        }
       }
       break;
+
+		// Rainbow
     case 'p':
       fill_rainbow(leds, NUM_LEDS, 0, 7);
       break;
+
+		// Black
     case 'x':
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Black;
       }
       break;
+
+		// ???	
     case '1':
       currentColor = 18;
       break;
@@ -165,29 +183,39 @@ void setup() {
     case '7' :
       currentColor = 24;
       break;
+
+	 // Default
    default:
       for (int i = 0; i < NUM_LEDS; i++) {
        leds[i] = CRGB::Red;
      }
      break;
   }  
+
+	// Driving LED data to controller
+	// Update controllers with current led colors
   FastLED.show();
 }
 
 void loop() {
+
   if (BTSerial.available()) {
-    char received = BTSerial.read();
+
+    char received = BTSerial.read();		// Define at top?
+
     if (received == 'r') {
-      for (int i = 0; i < NUM_LEDS; i++) {
+// Setting LEDs to mono red
+			for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Red;
       }
-      stopAnimation();
+      stopAnimation();					// ???
       currentColor = 0;
       FastLED.show();
       Serial.println("All LEDs are set to Red");
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;      
     } else if (received == 'g') {
+// Setting LEDs to mono green
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Green;
       }
@@ -197,6 +225,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;      
     } else if (received == 'b') {
+// Setting LEDs to mono blue
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Blue;
       }
@@ -215,6 +244,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received; 
     } else if (received == 'm') {
+// Setting LEDs to mono magenta
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Magenta;
       }
@@ -224,6 +254,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received; 
     } else if (received == 'v') {
+// Setting LEDs to mono violet
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Violet;
       }
@@ -233,6 +264,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'o') {
+// Setting LEDs to mono orange
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Orange;
       }
@@ -242,6 +274,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'y') {
+// Setting LEDs to mono yellow
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Yellow;
       }
@@ -251,6 +284,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'w') {
+// Setting LEDs to mono white
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::White;
       }
@@ -260,6 +294,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'q') {
+// Setting LEDs to dual yellow red
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Yellow;
@@ -274,6 +309,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'e') {
+// Setting LEDs to dual red blue
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Red;
@@ -288,6 +324,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 't') {
+// Setting LEDs to dual blue green
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Blue;
@@ -302,6 +339,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'z') {
+// Setting LEDs to dual magenta red
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Magenta;
@@ -316,6 +354,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'u') {
+// Setting LEDs to dual red green
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Red;
@@ -330,6 +369,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'i') {
+// Setting LEDs to dual yellow magenta
       for (int i = 0; i < NUM_LEDS; i++) {
        if (i >= 0 && i < 6) {
          leds[i] = CRGB::Yellow;
@@ -344,6 +384,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'p'){
+// Setting LEDs to rainbow
       fill_rainbow(leds, NUM_LEDS, 0, 7);
       currentColor = 15;
       FastLED.show();
@@ -352,6 +393,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == 'x') {
+// Setting LEDs to black
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::Black;
       }
@@ -361,6 +403,7 @@ void loop() {
       EEPROM.write(EEPROM_ADDRESS, received);
       lastColor = received;
     } else if (received == '1') {
+// Setting currentColor without explanation
       currentColor = 18;
       Serial.println("1");
       EEPROM.write(EEPROM_ADDRESS, received);
@@ -397,24 +440,33 @@ void loop() {
       lastColor = received;      
     }
   }
+// ================================
+	// Switch the following to cases?
+
   if(currentColor == 18){
+		// CHSV(uint8_t input_hue, uint8_t input_saturation,
+		//		  uint8_t input_value)
+		// input_value = brightness
+		// -> Representation of an HSV pixel
     leds[currentLed] = CHSV(millis() / 7, 255, 255);
     FastLED.show();
     currentLed = (currentLed + 1) % NUM_LEDS;
     delay(50);
   }
+
   if(currentColor == 19){
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hue + (i * 256 / NUM_LEDS), 255, 255);
+		for (int i = 0; i < NUM_LEDS; i++) {
+			leds[i] = CHSV(hue + (i * 256 / NUM_LEDS), 255, 255);
+		}
+		FastLED.show();
+		hue++;
+		if (hue >= 256) {
+			hue = 0;
+		}
+		fadeToBlackBy(leds, NUM_LEDS, fadeAmount);
+		delay(10);
   }
-  FastLED.show();
-  hue++;
-  if (hue >= 256) {
-    hue = 0;
-  }
-  fadeToBlackBy(leds, NUM_LEDS, fadeAmount);
-  delay(10);
-  }
+
   if(currentColor == 20){
     fadeToBlackBy(leds, NUM_LEDS, 10);
     for (int i = 0; i < NUM_LEDS; i++){
@@ -423,8 +475,9 @@ void loop() {
     FastLED.show();
     delay(30);
   }
+
   if (currentColor == 21){
-    static uint8_t hue = 0;
+    static uint8_t hue = 0;							// why defined again?
     for (int i = 0; i < NUM_LEDS; i++){
       leds[i] = CHSV(hue, 255, 255);
     }
@@ -432,6 +485,7 @@ void loop() {
     hue++;
     delay(10);
   }
+
   if (currentColor == 22){
     static uint8_t hue = 0;
     for (int i = 0; i < NUM_LEDS; i++){
@@ -446,6 +500,7 @@ void loop() {
       delay(Chase_Delay);
     }
   }
+
   if (currentColor == 23){
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     for (int i = 0; i < NUM_LEDS; i++) {
@@ -455,6 +510,7 @@ void loop() {
       currentLed23 = (currentLed23 + 1) % NUM_LEDS;
       leds[i] = CRGB::Black;
     }
+
     for (int j = 255; j >= 0; j -= fadeAmount23) {
       for (int k = 0; k < NUM_LEDS; k++) {
         leds[k].fadeToBlackBy(fadeAmount23);
@@ -464,28 +520,34 @@ void loop() {
       delay(delayTime);
     }
   }
+
   if (currentColor == 24){
-  CRGB wipeColor = CRGB::Blue;
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = wipeColor;
-    FastLED.show();
-    delay(50);
-  }
-  if(!animationRunning){
-    return;
-  }
-  wipeColor = CRGB::Green;
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = wipeColor;
-    FastLED.show();
-    delay(50);
-  }
-  if(!animationRunning){
-    return;
-  }
+  	CRGB wipeColor = CRGB::Blue;
+  	for(int i = 0; i < NUM_LEDS; i++) {
+    	leds[i] = wipeColor;
+    	FastLED.show();
+    	delay(50);
+  	}
+
+  	if(!animationRunning){				// ???
+    	return;
+		}
+		wipeColor = CRGB::Green;
+
+		for(int i = 0; i < NUM_LEDS; i++) {
+			leds[i] = wipeColor;
+			FastLED.show();
+			delay(50);
+		}
+
+		if(!animationRunning){
+			return;
+		}
+	}
 }
-}
-void stopAnimation(){
+
+// Needed? Not doing anything?
+void stopAnimation(){						// ???
   animationRunning = false;
 }
 
